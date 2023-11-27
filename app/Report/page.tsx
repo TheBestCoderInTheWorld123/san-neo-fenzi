94b15de95d9d43b12d188615e50013673da58a66
Siebel Suhail <72099341+TheBestCoderInTheWorld123@users.noreply.github.com>
2023-11-27 21:00:39 +0500

    Add files via upload

---
+++ app/Report/page.tsx
@@ -0,0 +1,91 @@
+"use client"
+import TablePage from "./table";
+import Navbar from "./navbar"
+import { Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+
+export default function Report() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('http://51.20.249.252:8000/get_device_latest_records')
+            .then(response => response.json())
+            .then(data => {
+                console.log()
+                const mytable = document.getElementById('data-container');
+                while (mytable.firstChild) {
+                    mytable.firstChild.remove();
+                }
+
+                for (const row of data.records) {
+                    const newRow = document.createElement('tr');
+                    var serialTD = document.createElement('td');
+                    serialTD.textContent = row.device_serial_number;
+                    var timeTD = document.createElement('td');
+                    timeTD.textContent = row.latest_recorded_date;
+                    var AQTD = document.createElement('td');
+                    AQTD.textContent = row.tags.AQ;
+                    var HUMTD = document.createElement('td');
+                    HUMTD.textContent = row.tags.HUM;
+                    var TMPTD = document.createElement('td');
+                    TMPTD.textContent = row.tags.TMP;
+
+                    newRow.appendChild(serialTD);
+                    newRow.appendChild(timeTD);
+                    newRow.appendChild(AQTD);
+                    newRow.appendChild(HUMTD);
+                    newRow.appendChild(TMPTD);
+                    mytable.appendChild(newRow);
+                }
+
+            });
+    }, []);
+    return (
+        <main>
+            {/* main div */}
+            <div>
+                <div id="sheet" className="px-4 py-4 flex justify-between items-center border-b">
+                    <Navbar />
+                </div>
+                {/* main page div */}
+                <div>
+                    {/* map & piechart */}
+                    <div>
+                        {/* map div */}
+                        <div></div>
+                        {/* piechart div */}
+                        <div></div>
+                    </div>
+                    {/* table div */}
+                    <div>
+                        <div className="TablePage">
+
+                            <Row>
+                                <Col>
+                                    <Card className="mb-3 px-6 ">
+                                        <CardHeader className="font-extrabold text-lg py-6">Table</CardHeader>
+                                        <div className="card card-body">
+                                            <table className="table table-hover w-full">
+                                                <thead>
+                                                    <th>Sr. No.</th>
+                                                    <th>Timestamp</th>
+                                                    <th>AQ</th>
+                                                    <th>HUM</th>
+                                                    <th>TMP</th>
+                                                </thead>
+                                                <tbody id="data-container"></tbody>
+                                            </table>
+                                        </div>
+                                    </Card>
+                                </Col>
+                            </Row>
+
+
+                        </div>
+                    </div>
+                </div>
+                {/*bottom bar */}
+                <div></div>
+            </div>
+        </main>
+    )
+};
