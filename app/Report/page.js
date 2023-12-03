3dadde0dcf3e33fd2331a18ea9f54535aedb00c6
BestCoderInTheWorld123 <siebel.suhail@gmail.com>
2023-12-04 01:57:25 +0500

    fixed API report

--- app/Report/page.js
+++ app/Report/page.js
@@ -5,62 +5,69 @@ import { Card, CardBody, CardHeader, Col, Row, Table, Button } from "reactstrap"
 import Select from "react-select";
 
 export default function Report() {
-  const options = [
-    { value: "device_serial_number", label: "Sr. No." },
-    { value: "latest_recorded_date", label: "Timestamp" },
-  ];
-
   const [data, setData] = useState([]);
   const [selectedColumn, setSelectedColumn] = useState("device_serial_number"); // Initialize selectedColumn state variable
   const [showInput, setShowInput] = useState(false);
   const [filterText, setFilterText] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  const [fromDateTime, setStartDateTime] = useState("");
+  const [toDateTime, setEndDateTime] = useState("");
   const [showTable, setShowTable] = useState(false);
+  // const [options, setOptions, setDeviceData] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [selectedSerialNumber, setSelectedSerialNumber] = useState('');
 
   useEffect(() => {
-    fetch("http://51.20.249.252:8000/get_device_latest_records")
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data.records);
-      });
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://51.20.249.252:8000/devices");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const device_data = await response.json();
+
+        // Since device_data is the array, map directly over it
+        if (Array.isArray(device_data)) {
+          const newOptions = device_data.map((device) => ({
+            value: device.device_serial_number,
+            label: device.device_serial_number
+          }));
+          setOptions(newOptions);
+          // setDeviceData(device_data); // Assuming you still want to set this state
+        } else {
+          console.log("device_data is not an array");
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
   }, []);
+
+  useEffect(() => {
+    console.log("Options updated:", options);
+  }, [options]);
+
+
+
+  // useEffect(() => {
+  //   fetch("http://51.20.249.252:8000/history_by_device_serial_no")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setData(data.records);
+  //     });
+  // }, []);
 
   const handleFilterChange = (event) => {
     setFilterText(event.target.value);
   };
 
-  const handleStartDateChange = (event) => {
-    setStartDate(event.target.value);
-  };
+  const handleStartDateTimeChange = (e) => setStartDateTime(e.target.value);
+  const handleEndDateTimeChange = (e) => setEndDateTime(e.target.value);
 
-  const handleEndDateChange = (event) => {
-    setEndDate(event.target.value);
-  };
 
-  const applyFilter = () => {
-    const filteredData = data.filter((item) => {
-      if (selectedColumn === "device_serial_number") {
-        return item.device_serial_number
-          .toLowerCase()
-          .includes(filterText.toLowerCase());
-      } else if (selectedColumn === "latest_recorded_date") {
-        const itemDate = new Date(item.latest_recorded_date);
-        const startDateObj = new Date(startDate);
-        const endDateObj = new Date(endDate);
 
-        return itemDate >= startDateObj && itemDate <= endDateObj;
-      } else {
-        // Filter for other columns (AQ, HUM, TMP)
-        return item[selectedColumn].toLowerCase().includes(filterText.toLowerCase());
-      }
-    });
-
-    // Update the table data with the filtered data
-    setData(filteredData);
-  };
-
-  const tableRows = data.map((row) => (
+  const tableRows = data ? data.map((row) => (
     <tr key={row.device_serial_number}>
       <td>{row.device_serial_number}</td>
       <td>{row.latest_recorded_date}</td>
@@ -68,11 +75,32 @@ export default function Report() {
       <td>{row.tags.HUM}</td>
       <td>{row.tags.TMP}</td>
     </tr>
-  ));
+  )) : null;
 
-  const showTableClick = () => {
-    setShowTable(!showTable);
+
+  const showTableClick = async () => {
+    // Construct the query parameters
+    console.log(fromDateTime, toDateTime);
+    const queryParams = new URLSearchParams({
+      sno: selectedSerialNumber, // The serial number selected from the dropdown
+      tdate: fromDateTime, // Assuming this state holds the 'to' date
+      fdate: toDateTime // Assuming this state holds the 'from' date
+    });
+
+    try {
+      const response = await fetch(`http://51.20.249.252:8000/history_by_device_serial_no?${queryParams}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const historyData = await response.json();
+      console.log(historyData);
+      setData(historyData.records); // Be sure this is the correct place to set data
+      setShowTable(true); // Update state to show table
+    } catch (error) {
+      console.error('Error fetching history data:', error);
+    }
   };
+
 
   return (
     <main>
@@ -85,94 +113,62 @@ export default function Report() {
         {/* Main page div */}
         <div>
           <div id="TablePage">
-            <div className="flex p-6">
-              <button id="tablebutton" onClick={() => showTableClick()}>
-                Show Table
-              </button>
-            </div>
-
-            <div className="flex-col mb-3">
+            <div className="flex-col m-4">
               <div className="flex items-center mr-4">
-                <label className="mr-2">Filter by:</label>
+                <label className="mr-2">Sr. No.</label>
                 <Select
                   className="filter-select"
-                  value={selectedColumn}
-                  onChange={(event) => setSelectedColumn(event.target.value)}
+                  value={options.find(option => option.value === selectedColumn)}
+                  onChange={(option) => setSelectedColumn(option.value)}
                   options={options}
-                >
-                  {options.map((option) => (
-                    <option
-                      className="filter-select" key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))}
-                </Select>
+                />
+                <label>Start Date and Time:</label>
+                <input
+                  className="filter-date"
+                  type="datetime-local"
+                  value={fromDateTime}
+                  onChange={handleStartDateTimeChange}
+                />
+
+                <label>End Date and Time:</label>
+                <input
+                  className="filter-date"
+                  type="datetime-local"
+                  value={toDateTime}
+                  onChange={handleEndDateTimeChange}
+                />
+
+                <button className="tablebutton" onClick={() => showTableClick()} >
+                  Show Data
+                </button>
               </div>
 
-              {showInput && (
-                <div className="flex items-center mr-4">
-                  <input
-                    className="filter-input"
-                    type="text"
-                    placeholder="Enter filter text"
-                    value={filterText}
-                    onChange={handleFilterChange}
-                  />
-                </div>
-              )}
+              <div>
+                {showTable && (
+                  <Row>
+                    <Col>
+                      <Card className="mb-3 px-6">
+                        <CardHeader className="font-extrabold text-lg py-6">Table</CardHeader>
 
-              {selectedColumn === "latest_recorded_date" && (
-                <div className="flex items-center mr-4">
-                  <label className="mr-2">Start Date:</label>
-                  <input
-                    className="filter-date"
-                    type="date"
-                    value={startDate}
-                    onChange={handleStartDateChange}
-                  />
-                </div>
-              )}
-
-              {selectedColumn === "latest_recorded_date" && (
-                <div className="flex items-center mr-4">
-                  <label className="mr-2">End Date:</label>
-                  <input
-                    className="filter-date"
-                    type="date"
-                    value={endDate}
-                    onChange={handleEndDateChange}
-                  />
-                </div>
-              )}
-
-              <Button className="filter-button" color="primary" onClick={applyFilter}>
-                Filter
-              </Button>
-
-              {showTable && (
-                <Row>
-                  <Col>
-                    <Card className="mb-3 px-6">
-                      <CardHeader className="font-extrabold text-lg py-6">Table</CardHeader>
-
-                      <div id="TablePage" className="card card-body">
-                        <table id="TablePage" className="table table-hover w-full">
-                          <thead>
-                            <tr>
-                              <th>Sr. No.</th>
-                              <th>Timestamp</th>
-                              <th>AQ</th>
-                              <th>HUM</th>
-                              <th>TMP</th>
-                            </tr>
-                          </thead>
-                          <tbody>{tableRows}</tbody>
-                        </table>
-                      </div>
-                    </Card>
-                  </Col>
-                </Row>
-              )}
+                        <div id="TablePage" className="card card-body">
+                          <table id="TablePage" className="table table-hover w-full">
+                            <thead>
+                              <tr>
+                                <th>Sr. No.</th>
+                                <th>Timestamp</th>
+                                <th>AQ</th>
+                                <th>HUM</th>
+                                <th>TMP</th>
+                              </tr>
+                            </thead>
+                            <tbody>{tableRows}</tbody>
+                          </table>
+                        </div>
+                      </Card>
+                    </Col>
+                  </Row>
+                )}
+              </div>
             </div>
           </div>
         </div>
