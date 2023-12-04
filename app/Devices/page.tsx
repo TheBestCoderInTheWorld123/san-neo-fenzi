a6e5ef31a29e8f5b1a0ac36c0b7016ad8cb5a6fd
hajikhanp <hajikhan.p@gmail.com>
2023-12-04 21:25:22 +0530

    removed extra code and added label

--- app/Devices/page.tsx
+++ app/Devices/page.tsx
@@ -7,43 +7,7 @@ import { Card, CardBody, CardHeader, Col, Row, Table, Button } from "reactstrap"
 export default function Devices() {
     const [data, setData] = useState([]);
     useEffect(() => {
-        // fetch('http://51.20.249.252:8000/get_device_latest_records')
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         console.log()
-        //         const mytable = document.getElementById('data-container');
-        //         if (mytable) {
-        //             while (mytable.firstChild) {
-        //                 mytable.firstChild.remove();
-        //             }
-        //         }
-
-
-
-        //         for (const row of data.records) {
-        //             const newRow = document.createElement('tr');
-        //             var serialTD = document.createElement('td');
-        //             serialTD.textContent = row.device_serial_number;
-        //             var timeTD = document.createElement('td');
-        //             timeTD.textContent = row.latest_recorded_date;
-        //             var AQTD = document.createElement('td');
-        //             AQTD.textContent = row.tags.AQ;
-        //             var HUMTD = document.createElement('td');
-        //             HUMTD.textContent = row.tags.HUM;
-        //             var TMPTD = document.createElement('td');
-        //             TMPTD.textContent = row.tags.TMP;
-
-        //             newRow.appendChild(serialTD);
-        //             newRow.appendChild(timeTD);
-        //             newRow.appendChild(AQTD);
-        //             newRow.appendChild(HUMTD);
-        //             newRow.appendChild(TMPTD);
-        //             if (mytable) {
-        //                 mytable.appendChild(newRow);
-        //             }
-        //         }
-
-        //     });
+        
     }, []);
     return (
         <main>
@@ -53,7 +17,10 @@ export default function Devices() {
                     <Row>
                         <Col>
                             <Card className="mb-3 px-6">
-                                <CardHeader className="font-extrabold text-lg py-6">Table</CardHeader>
+                            <div className="font-extrabold text-lg py-6 text-center w-full"> {/* Center text */}
+                            <h4>Devices Latest Data</h4>
+                            </div>
+  
                                 <div id="TablePage" className="card card-body">
                                     <TablePage />
                                 </div>
