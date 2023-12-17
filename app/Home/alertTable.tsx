5d4c7e20262944058b27af4237fefcb172352833
hajikhanp <hajikhan.p@gmail.com>
2023-12-18 02:13:12 +0530

    alert table code changes

--- app/Home/alertTable.tsx
+++ app/Home/alertTable.tsx
@@ -1,38 +1,69 @@
-import { Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Row } from 'reactstrap';
+
+interface Alert {
+    id: number;
+    time: string;
+    alert_type: string;
+    device_serial_num: string;
+}
 
 export default function AlertTable() {
+    const [alerts, setAlerts] = useState<Alert[]>([]);
+
+    useEffect(() => {
+        const fetchData = () => {
+            fetch('http://51.20.249.252:8000/latest_alerts')
+                .then(response => response.json())
+                .then(data => {
+                    const sortedData = data.sort((a: Alert, b: Alert) => new Date(b.time).getTime() - new Date(a.time).getTime());
+                    setAlerts(sortedData);
+                })
+                .catch(error => console.error('Error fetching data:', error));
+        };
+
+        fetchData(); // Fetch data initially
+        const interval = setInterval(fetchData, 15000); // Fetch data every 15 seconds
+
+        return () => clearInterval(interval); // Cleanup interval on component unmount
+    }, []);
+
+    const formatDate = (dateString: string) => {
+        const options: Intl.DateTimeFormatOptions = {
+            year: 'numeric', 
+            month: '2-digit', 
+            day: '2-digit', 
+            hour: '2-digit', 
+            minute: '2-digit', 
+            second: '2-digit', 
+            hour12: false
+        };
+        return new Date(dateString).toLocaleString('en-GB', options).replace(/\//g, '-').replace(/, /g, ' ');
+    };
+
     return (
-                
         <Row>
-            {/* <div className="font-extrabold text-lg mt-2 py-6 text-center w-full"> 
-            {/* Center text */}
-                    {/* <p id="text">Alert Data</p> */}
-        {/* </div> */} 
-        
             <Col>
                 <Card className="mb-3 px-6 ">
-                    <div className="card card-body">
+                    <div className="card card-body" style={{ maxHeight: '400px', overflowY: 'scroll' }}>
                         <table className="table table-hover w-full">
                             <thead className="bg-blue-200">
-                                <th>Alert ID</th>
-                                <th>Reported At</th>
-                                <th>Alert Type</th>
-                                <th>Device Sr No</th>
+                                <tr>
+                                    <th>Alert ID</th>
+                                    <th>Reported At</th>
+                                    <th>Alert Type</th>
+                                    <th>Device Sr No</th>
+                                </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>A00001</td>
-                                    <td>2023-12-05 01:35:00</td>
-                                    <td>Critical</td>
-                                    <td>862174069140333</td>
-                                    
-                                </tr>
-                                <tr>
-                                    <td>A00002</td>
-                                    <td>2023-12-03 01:35:00</td>
-                                    <td>Normal</td>
-                                    <td>862174069141333</td>
-                                </tr>
+                                {alerts.map(alert => (
+                                    <tr key={alert.id}>
+                                        <td>{alert.id}</td>
+                                        <td>{formatDate(alert.time)}</td>
+                                        <td>{alert.alert_type}</td>
+                                        <td>{alert.device_serial_num}</td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
