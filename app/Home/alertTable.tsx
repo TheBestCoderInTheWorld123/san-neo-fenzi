2e9e59eaa48728ff714c3dfee414e691eecfb28f
hajikhanp <hajikhan.p@gmail.com>
2023-12-18 19:26:25 +0530

    app.py for taking latest 10 records

--- app/Home/alertTable.tsx
+++ app/Home/alertTable.tsx
@@ -6,24 +6,28 @@ interface Alert {
     time: string;
     alert_type: string;
     device_serial_num: string;
+    tag_name: string;
+    tag_value: number;
 }
 
 export default function AlertTable() {
     const [alerts, setAlerts] = useState<Alert[]>([]);
 
     useEffect(() => {
-        const fetchData = () => {
-            fetch('http://51.20.249.252:8000/latest_alerts')
-                .then(response => response.json())
-                .then(data => {
-                    const sortedData = data.sort((a: Alert, b: Alert) => new Date(b.time).getTime() - new Date(a.time).getTime());
-                    setAlerts(sortedData);
-                })
-                .catch(error => console.error('Error fetching data:', error));
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://51.20.249.252:8000/latest_alerts');
+                const data = await response.json();
+                const sortedData = data.sort((a: Alert, b: Alert) => new Date(b.time).getTime() - new Date(a.time).getTime());
+                console.log(sortedData);
+                setAlerts(sortedData);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         };
 
         fetchData(); // Fetch data initially
-        const interval = setInterval(fetchData, 15000); // Fetch data every 15 seconds
+        const interval = setInterval(fetchData, 5000); // Fetch data every 15 seconds
 
         return () => clearInterval(interval); // Cleanup interval on component unmount
     }, []);
@@ -44,7 +48,7 @@ export default function AlertTable() {
     return (
         <Row>
             <Col>
-                <Card className="mb-3 px-6 ">
+                <Card className="mb-3 px-6">
                     <div className="card card-body" style={{ maxHeight: '400px', overflowY: 'scroll' }}>
                         <table className="table table-hover w-full">
                             <thead className="bg-blue-200">
@@ -53,6 +57,8 @@ export default function AlertTable() {
                                     <th>Reported At</th>
                                     <th>Alert Type</th>
                                     <th>Device Sr No</th>
+                                    <th>Tag Name</th>
+                                    <th>Tag Value</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -62,6 +68,8 @@ export default function AlertTable() {
                                         <td>{formatDate(alert.time)}</td>
                                         <td>{alert.alert_type}</td>
                                         <td>{alert.device_serial_num}</td>
+                                        <td>{alert.tag_name}</td>
+                                        <td>{alert.tag_value}</td>
                                     </tr>
                                 ))}
                             </tbody>
