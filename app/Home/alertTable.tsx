3d4cd88a28c23d3dffc0b081d39feecb486b7d57
hajikhanp <hajikhan.p@gmail.com>
2023-12-18 20:32:01 +0530

    alert table changes and remove console.log

--- app/Home/alertTable.tsx
+++ app/Home/alertTable.tsx
@@ -12,6 +12,8 @@ interface Alert {
 
 export default function AlertTable() {
     const [alerts, setAlerts] = useState<Alert[]>([]);
+    const [latestId, setLatestId] = useState<number | null>(null);
+    const [blink, setBlink] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -19,8 +21,13 @@ export default function AlertTable() {
                 const response = await fetch('http://51.20.249.252:8000/latest_alerts');
                 const data = await response.json();
                 const sortedData = data.sort((a: Alert, b: Alert) => new Date(b.time).getTime() - new Date(a.time).getTime());
-                console.log(sortedData);
+                //console.log(sortedData);
                 setAlerts(sortedData);
+                if (sortedData.length > 0 && sortedData[0].id !== latestId) {
+                    setLatestId(sortedData[0].id);
+                    setBlink(true);
+                    setTimeout(() => setBlink(false), 5000); // Stop blinking after 5 seconds
+                }
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -30,19 +37,27 @@ export default function AlertTable() {
         const interval = setInterval(fetchData, 5000); // Fetch data every 15 seconds
 
         return () => clearInterval(interval); // Cleanup interval on component unmount
-    }, []);
+    },  [latestId]);
 
     const formatDate = (dateString: string) => {
-        const options: Intl.DateTimeFormatOptions = {
-            year: 'numeric', 
-            month: '2-digit', 
-            day: '2-digit', 
-            hour: '2-digit', 
-            minute: '2-digit', 
-            second: '2-digit', 
-            hour12: false
-        };
-        return new Date(dateString).toLocaleString('en-GB', options).replace(/\//g, '-').replace(/, /g, ' ');
+        const [date, timeWithTimeZone] = dateString.split('T');
+        const [time] = timeWithTimeZone.split('+');
+    
+        // Combine date and time for the final format
+        return `${date} ${time}`;
+    };
+    const getAlertTypeClassName = (alertType: string) => {
+        switch (alertType) {
+            case 'CRITICAL':
+                return 'alert-critical';
+            case 'NORMAL':
+                return 'alert-normal';
+            default:
+                return '';
+        }
+    };
+    const isLatestRecord = (index: number) => {
+        return index === 0; // Assuming the first record is the latest
     };
 
     return (
@@ -62,10 +77,11 @@ export default function AlertTable() {
                                 </tr>
                             </thead>
                             <tbody>
-                                {alerts.map(alert => (
-                                    <tr key={alert.id}>
+                                {alerts.map((alert, index) => (
+                                    <tr  className={index === 0 && blink ? 'blinking' : ''} key={alert.id}>
                                         <td>{alert.id}</td>
                                         <td>{formatDate(alert.time)}</td>
+                                        {/* <td>{alert.alert_type}</td> */}
                                         <td>{alert.alert_type}</td>
                                         <td>{alert.device_serial_num}</td>
                                         <td>{alert.tag_name}</td>
